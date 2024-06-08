864226a69958f1be124d7128dea613428833f89f
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-08 16:14:31 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -70,9 +70,9 @@ class BasicAuth(Auth):
         """ A method that returns the User instance
         based on his email and password.
         """
-        if user_email is None:
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if user_pwd is None:
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
         users = User.search(email=user_email)
         if not users:
