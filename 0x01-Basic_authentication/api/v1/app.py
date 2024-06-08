781d57ede8cb2b018a2d15a4597de4c644b3bddb
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-08 14:15:09 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,7 +11,6 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -21,6 +20,7 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
