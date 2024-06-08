a6cf1264f7950dde6fd8a3eee271b8d18e17787b
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-08 14:21:25 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,8 +9,8 @@ class BasicAuth(Auth):
     """
 
     def extract_base64_authorization_header(
-        self, authorization_header: str
-        )-> str:
+    self, authorization_header: str
+    ) -> str:
         """returns the Base64 part of the Authorization
         header for a Basic Authentication
         """
