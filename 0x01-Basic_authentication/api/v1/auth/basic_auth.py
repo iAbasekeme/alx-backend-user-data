9dc72f6085d1ca636a397f6459daebb4df0bc1fd
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-08 14:42:02 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,6 +32,8 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
             self,
             base64_authorization_header: str) -> str:
+        """A method that decodes a base64 algorithm
+        """
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
