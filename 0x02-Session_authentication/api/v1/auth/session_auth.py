f5e61bbfaafd3054a1fdffd99cc3ef8188e96dcb
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-09 12:28:52 +0100

    task_1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A module that handles session authentication implementations
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class that implements session authentication
+    """
+    pass
