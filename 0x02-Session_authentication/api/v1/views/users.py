4a9d52e2961f700f2087acf4ccbbf0a27ee27311
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-09 12:11:37 +0100

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -38,13 +38,6 @@ def view_one_user(user_id: str = None) -> str:
     return jsonify(user.to_json())
 
 
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
-def get_user() -> str:
-    """A route that retrieves the authenticated user
-    """
-    return jsonify(request.current_user.to_json())
-
-
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
