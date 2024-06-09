26c0fcf3382d454483869dfd0d8ea62c98af6df6
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-09 12:46:55 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,16 @@
 """A module that handles session authentication implementations
 """
 from .auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """A class that implements session authentication
     """
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
