a1b25ee1ccccee4386df485bc7e23b01b5f5f45a
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-09 14:59:32 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,25 +10,31 @@ from typing import Tuple
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> Tuple[str, int]:
-    """A POST route that handles login for a user and
-    creates a session for them
+def login():
+    """ POST /auth_session/login
+    Return:
+      - Response
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if not email:
-        return jsonify({"error": "email missing"}), 400
-    if not password:
-        return jsonify({"error": "password missing"}), 400
-    users = User.search({'email': email})
-    if not users:
-        return jsonify({"error": "no user found for this email"}), 404
-    user = users[0]
-    passw = user.is_valid_password(password)
-    if passw:
-        from api.v1.app import auth
-        session_id = auth.create_session(getattr(user, id))
-        res = jsonify(user.to_json())
-        res.set_cookie(getenv("SESSION_NAME"), session_id)
-        return res
-    return jsonify({"error": "wrong password"}), 401
+    user_email = request.form.get('email')
+    user_pwd = request.form.get('password')
+    if not user_email:
+        return jsonify(error="email missing"), 400
+    if not user_pwd:
+        return jsonify(error="password missing"), 400
+    try:
+        user = User.search({"email": user_email})
+    except Exception:
+        return jsonify(error="no user found for this email"), 404
+    if not user:
+        return jsonify(error="no user found for this email"), 404
+    for u in user:
+        if u.is_valid_password(user_pwd):
+            user_id = u.id
+            from api.v1.app import auth
+            session_id = auth.create_session(user_id)
+            response = jsonify(u.to_json())
+            response.set_cookie(getenv('SESSION_NAME'), session_id)
+            return response
+        else:
+            return jsonify(error="wrong password"), 401
+    return jsonify(error="no user found for this email"), 404
