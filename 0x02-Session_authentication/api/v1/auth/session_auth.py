8f87e67c8b76abb7594b77b11e3d454d4b61b991
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-09 12:48:30 +0100

    fixed pycodesyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,14 +4,15 @@
 from .auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """A class that implements session authentication
     """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-
