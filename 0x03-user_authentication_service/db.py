6c8c5a2abbb77a11c9906ac70a0e7f23258d7696
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-13 19:40:41 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,7 +31,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """A method that saves a user to the database
+        """
+        add a new user
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
