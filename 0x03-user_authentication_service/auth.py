1ff31b8c7301dae7d0293d59ac0e2b6d0dbddae9
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-14 00:20:49 +0100

    Create auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A module that tackles authentication
+"""
+import bcrypt
+
+
+def _hash_password(password: str):
+    """A function that takes a password and hashes the password
+    """
+    if password:
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode(), salt)
+        return hashed_password
