fec24744160dd406c1b2c75f23bd54c1cdc16836
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-14 00:48:20 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """ A module that tackles authentication
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +12,19 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError('User ${email} already exists')
+        else:
+            hashed_password =  _hash_password(password)
+            self._db.add_user(email, hashed_password)
