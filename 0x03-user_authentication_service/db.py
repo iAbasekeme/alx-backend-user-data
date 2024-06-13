54a17a9e216b9166304b44cc9e7d34b9d81dc02d
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-13 23:58:28 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -56,7 +56,8 @@ class DB:
         try:
             user = self.find_user_by(id=user_id)
             for key, value in kwargs.items():
-                setattr(user, key, value)
+                if hasattr(user, key):
+                    setattr(user, key, value)
             self.__session.commit()
         except ValueError:
             raise ValueError
