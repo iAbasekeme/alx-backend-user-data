2c00b788c9e55862ce89d9c2b4b4814c5ca2f18d
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-14 01:21:55 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """A module that holds a flask application
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -11,6 +15,21 @@ def start():
     """A mehod that handles the root of a route
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register():
+    """A rouet that implements registering users and saving them.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email or password is None:
+        return jsonify({"message": "Input your email and password"}), 400
+    try:
+        new_user = Auth.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
