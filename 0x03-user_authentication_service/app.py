f0f25a7b40e24f528bf4b70ec51a652049d9953a
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-14 01:06:06 +0100

    Create app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A module that holds a flask application
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+app.route('/', methods=['GET'])
+
+
+def start():
+    """A mehod that handles the root of a route
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
